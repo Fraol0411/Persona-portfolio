1b23f0e92c181e7d6e46a7995daec2e3992bc5bb
Fraol0411 <Fraol0411@gmail.com>
2024-12-24 13:49:18 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import './App.scss';
-import {About,Footer,Header,Skills,Works,Testimony, Footerr} from './Container';
-import Navbar from './Components/Navbar/Navbar';
-import Certificate from './Container/Certificate/Certificate';
+import "./App.scss";
+import {
+  About,
+  Footer,
+  Header,
+  Skills,
+  Works,
+  Testimony,
+  Footerr,
+} from "./Container";
+import Navbar from "./Components/Navbar/Navbar";
+import Certificate from "./Container/Certificate/Certificate";
 
 function App() {
-
   return (
-        <div className="app">
-           <Navbar/>
-           <Header/>
-           <About/>
-           <Works/>
-           <Certificate/>
-           <Skills/>        
-           <Footer/>
-        </div>  
-  ); 
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Works />
+      {/* <Certificate/> */}
+      <Skills />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/Container/Header/Header.jsx
+++ src/Container/Header/Header.jsx
@@ -1,11 +1,11 @@
-import React,{useNavigate, useState} from 'react'
-import "./Header.scss"
+import React, { useNavigate, useState } from "react";
+import "./Header.scss";
 // import { AppWrap } from '../../wrapper/Appwrap';
-import {motion} from 'framer-motion'
-import {images} from "../../Constants"
-import { useEffect } from 'react';
+import { motion } from "framer-motion";
+import { images } from "../../Constants";
+import { useEffect } from "react";
 // import {urlFor,client} from '../../client'
-import Images from '../../Constants/Images';
+import Images from "../../Constants/Images";
 
 const scaleVariants = {
   whileInView: {
@@ -13,13 +13,13 @@ const scaleVariants = {
     opacity: [0, 1],
     transition: {
       duration: 1,
-      ease: 'easeInOut',
+      ease: "easeInOut",
     },
   },
 };
 function Header() {
-  const [proimg,setProimg]=useState([])
- 
+  const [proimg, setProimg] = useState([]);
+
   // useEffect(()=>{
   //   const query='*[_type == "header"]'
 
@@ -27,58 +27,48 @@ function Header() {
   //   .then((data)=>setProimg(data))
   // })
 
-  console.log(proimg.img)
+  console.log(proimg.img);
   return (
-    <div id='Home' className='app__header ' >
-        
-        <motion.div
-         whileInView={{y:[0,60],opacity:[0,1]}}
-         transition={{duration:2}}
-         className="app__header-info"
+    <div id="Home" className="app__header ">
+      <motion.div
+        whileInView={{ y: [0, 60], opacity: [0, 1] }}
+        transition={{ duration: 2 }}
+        className="app__header-info"
       >
-        {/* <div className='app__header-badge'>
-          <div className="badge-cmp ">
-             
-             <div style={{marginLeft:20}}>
-                 <span className='p-text'>Wellcome</span>
-                 <p className="p_text">Hello,I am</p>
-                <h1 className="headtext">Fraol Dabi</h1>
-             </div>     
-          </div>
-          <div className='tag-camp'>
-               <h2 className="head-text1">React-js Front<span>End Web Developer</span></h2>
-               <h2 className="head-text1">UX/UI <span>Designer</span></h2>
-               <h2 className="head-text1">FREE<span>Ler</span></h2>
-               
-          </div>
-        </div> */}
         <div className="textpart">
-          <h1>Hi, am Fraol <span>Dabi</span></h1>
-          <p>Official portfolio website <br/>showcase skills and exprience <br/> </p>
-          <h2 className="head-text1">Java-script Front<span>End Web Developer</span></h2>
-          <h2 className="head-text1">Python <span>Developer</span></h2>
-          <h2 className="head-text1">UX/UI <span>Designer</span></h2>
-          <a href='#Contact me'>Contact Me</a>
+          <h1>
+            Hi, am Kidist <span>Tesfaye</span>
+          </h1>
+          <p>
+            Official portfolio website <br />
+            showcase skills and exprience <br />{" "}
+          </p>
+          <h2 className="head-text1">
+            Java-script Front<span>End Web Developer</span>
+          </h2>
+          <h2 className="head-text1">
+            Python <span>Developer</span>
+          </h2>
+          <h2 className="head-text1">
+            UX/UI <span>Designer</span>
+          </h2>
+          <a href="#Contact me">Contact Me</a>
         </div>
       </motion.div>
 
       <motion.div
-         whileInView={{x:[80,0],opacity:[0,1]}}
-         transition={{duration:2,delayChildren:0.5}}
-         className="app__header-image"
-      > 
-      
-           
-             <>
-              <img src={images.shape} alt="shape" className='shape'/>
-              <img src={images.bgpro} alt="nothing" className='profile' />
-              {/* <img src={urlFor(image.img)} alt="profile" className='profile'/>   */}
-             </>
-     
-
-      </motion.div>        
+        whileInView={{ x: [80, 0], opacity: [0, 1] }}
+        transition={{ duration: 2, delayChildren: 0.5 }}
+        className="app__header-image"
+      >
+        <>
+          <img src={images.shape} alt="shape" className="shape" />
+          <img src={images.nobgprokidi} alt="nothing" className="profile" />
+          {/* <img src={urlFor(image.img)} alt="profile" className='profile'/>   */}
+        </>
+      </motion.div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
